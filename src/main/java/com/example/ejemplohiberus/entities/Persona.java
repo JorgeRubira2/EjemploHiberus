95fe45779fb9bc5b5cb17c9fd1546a1d6adeabe3
Jorge Rubira Santos <jorgesub@gmail.com>
2021-09-28 09:58:19 +0200

    Creación de repositories

---
+++ src/main/java/com/example/ejemplohiberus/entities/Persona.java
@@ -0,0 +1,21 @@
+
+package com.example.ejemplohiberus.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@Entity
+@Table(name = "personas")
+@Data
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+}
