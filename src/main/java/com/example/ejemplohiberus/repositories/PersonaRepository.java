d7b6cd4b260f50e338bd3ad97060925e82e6056f
Jorge Rubira Santos <jorgesub@gmail.com>
2021-09-28 10:03:00 +0200

    Creación de repositories

--- src/main/java/com/example/ejemplohiberus/repositories/PersonaRepository.java
+++ src/main/java/com/example/ejemplohiberus/repositories/PersonaRepository.java
@@ -2,7 +2,7 @@ package com.example.ejemplohiberus.repositories;
 
 import com.example.ejemplohiberus.entities.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 public interface PersonaRepository extends JpaRepository<Persona, Integer>{
 }
